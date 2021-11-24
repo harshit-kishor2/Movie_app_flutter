7783e8cc5eada90b0b66f2e3f2a95cd692cbcd9a
Harshit Kishor <harshitkishore@singsys.com>
2021-11-24 13:58:07 +0530

    completed

--- lib/core/base/usecase.dart
+++ lib/core/base/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'base_failure.dart';
+import 'package:movie_app/core/base/base_failure.dart';
 
 // Parameters have to be put into a container object so that they can be
 // included in this abstract base class method definition.
